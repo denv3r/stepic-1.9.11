e48c8e08103f4e0f48598f4b14c7a00deb6dca3a
denv3r <misterdenver@gmail.com>
2016-05-14 02:23:08 +0300

    add config to launch parameters

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/gunicorn stop
-sudo gunicorn hello:wsgi_application
+sudo gunicorn -c gunicorn.conf.py hello:wsgi_application
