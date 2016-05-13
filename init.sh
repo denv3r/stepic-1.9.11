a07bf70c9365d5e2614055f9926f70e4b1991f48
denv3r <misterdenver@gmail.com>
2016-05-14 02:24:41 +0300

    add nginx config

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/gunicorn stop
