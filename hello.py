ffc2b8f83a8f3b8897e0224a9ac77ff2b1f5e505
denv3r <misterdenver@gmail.com>
2016-05-14 02:37:53 +0300

    for python2

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-import urllib.parse
+# import urllib.parse
+import urlparse
 
 def wsgi_application(environ, start_response):
 
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    query_dict = urllib.parse.parse_qs(environ['QUERY_STRING'])
+    # query_dict = urllib.parse.parse_qs(environ['QUERY_STRING'])
+    query_dict = urlparse.parse_qs(environ['QUERY_STRING'])
     body = []
     for param, value in query_dict.items():
         print(param + '=' + value[0])
--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/gunicorn stop
-sudo gunicorn -c gunicorn.conf.py hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:wsgi_application
