14b207cc686d53449778b2c40cd3cc470328b860
denv3r <misterdenver@gmail.com>
2016-05-14 03:18:23 +0300

    last

--- hello.py
+++ hello.py
@@ -1,15 +1,11 @@
-# import urllib.parse
-import urlparse
-
 def wsgi_application(environ, start_response):
 
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    # query_dict = urllib.parse.parse_qs(environ['QUERY_STRING'])
-    query_dict = urlparse.parse_qs(environ['QUERY_STRING'])
+    query = environ['QUERY_STRING'].split('&')
     body = ''
-    for param, value in query_dict.items():
-        body += param + '=' + value[0] + '\n'
+    for param in query:
+        body += param + '\n'
     print body
     
     start_response(status, headers)
